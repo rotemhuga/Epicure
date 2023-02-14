0c745d72e7e75f07e54401ff4e01248c4850f5af
rotem huga <rotem.3007@gmail.com>
2023-02-14 22:17:08 +0200

    adding the all chef page - without style

--- src/component/All_chefs/All_chefs.tsx
+++ src/component/All_chefs/All_chefs.tsx
@@ -1,11 +1,59 @@
 import react from "react"
+import CardWeekChef from "../Card_week_chef/Card_week_chef"
+import data from "../../epicure.json"
 
 const All_chefs:React.FC = () => {
-    return (
-        <div>
-            
+    const chefs = data.chefs.map((chef:any) => {
+        return <CardWeekChef 
+        class= {chef.name}
+        // srcChef= {chefImg}
+        // srcChef= {require(`../../assets/images/Chefs/${chef.portrait}.png`)}
+        ChefName = {chef.name}  
+              />  
+})
+    return ( 
+        <div className="all-chef-page" >
+            <div className="all-chef-page-container">
+                {chefs}
+            </div>
         </div>
-    )
+    ) 
 }
 
 export default All_chefs
+
+{/* const Week_chef: React.FC = ()=> {
+    const items = data.chefs.map((item:any) => {
+        if (item.isChefOfTheWeek === true) {
+            return < CardWeekChef
+            class= {item.name}
+            srcChef= {require(`../../../assets/images/${item.portrait}.png`)}
+            ChefName = {item.name}
+            info = {item.info}
+            restTitle = {`${item.firstName}'s Restaurants`}
+            // restaurants= {item.restaurants}
+            restaurant1 = {item.restaurants[0]} 
+            srcRest1 = {require(`../../../assets/images/Restaurants/${item.restaurantsImg[0]}.png`)}
+            restaurant2 = {item.restaurants[1]} 
+            srcRest2 = {require(`../../../assets/images/Restaurants/${item.restaurantsImg[1]}.png`)}
+            restaurant3 = {item.restaurants[2]} 
+            srcRest3 = {require(`../../../assets/images/Restaurants/${item.restaurantsImg[2]}.png`)}
+            />
+        
+        } else {
+            return null;
+        }   
+    })
+ 
+    return (
+        <div id="week-chef-container">
+            <div id="week-chef-title">CHEF OF THE WEEK:</div>
+            <div id="week-chef-all-content">
+                {items}
+            </div>
+        </div>
+    ) 
+
+}
+
+export default Week_chef */}
--- src/component/ChefsPage/ChefsPage.tsx
+++ src/component/ChefsPage/ChefsPage.tsx
@@ -3,12 +3,14 @@ import "./ChefsPage.css"
 import Navbar from "../navbar/all_navbar/navbar";
 import Footer from "../General/Footer/Footer";
 import FilterChefPage from "../General/Filter_chef_page/Filter_chef_page"
+import All_chefs from "../All_chefs/All_chefs"
 
 const ChefsPage: React.FC = () => {
     return (
         <div id="Chefs-page">
             <Navbar />
             <FilterChefPage />
+            <All_chefs />
             <Footer />
         </div>
     );
