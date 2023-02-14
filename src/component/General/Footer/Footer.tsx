010b168b10dd48e6617f0550c22961bd94995555
rotem huga <rotem.3007@gmail.com>
2023-02-14 16:02:44 +0200

    editing the restaurant page - 14.02

--- src/component/General/Footer/Footer.tsx
+++ src/component/General/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import "../Footer/Footer.css"
 
 const Footer: React.FC = ()=> {
     return (
-        <div id="footer-container">
+        <footer id="footer-container">
             <button className="contact-us footer-button">Contact us</button>
             <button className="terms footer-button">Term of Use</button>
             <button className="privacy footer-button">Privacy Policy</button>
-        </div>
+        </footer>
     )
 }
 export default Footer
