4de7f120f102f7c24a6c4c9d6500b920b33679e2
rotem huga <rotem.3007@gmail.com>
2023-03-12 23:05:20 +0200

    working on the sign up - 12.03

--- back/src/services/signUp.service.ts
+++ back/src/services/signUp.service.ts
@@ -1,12 +1,18 @@
 import {signUpModel} from "../model/signUp.model"
 import { ISignUpBack } from "../model/signUp.model";
+import express, {Request, Response} from "express";
+
+
 
 export const getSignUp = async (data:any) => {
-const _signUp = new signUpModel(data);
+const _signUp = new signUpModel();
+
 try {
         console.log(_signUp.emailAddress)
         const checkEmail = signUpModel.find({emailAddress:_signUp.emailAddress})
-        console.log(checkEmail)
+        // if (checkEmail) {
+        //     return res.status(409).send("User Already Exist. Please Login");
+        //   }
         await _signUp.save();
         return _signUp
     } catch (e) {
