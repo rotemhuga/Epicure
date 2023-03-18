c15dde7fd03f4180422fadb39bb0823ad873a33c
rotem huga <rotem.3007@gmail.com>
2023-03-18 15:03:18 +0200

    finishing add restaurant modal - 18.03

--- front/src/component/General/Filter_rest_options/Filter_rest_options.tsx
+++ front/src/component/General/Filter_rest_options/Filter_rest_options.tsx
@@ -17,12 +17,6 @@ const toggleModal = () => {
     setModal(!modal);
   };
 
-  // const [id, setId] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [userName, setUserName] = useState("");
-  // const [password, setPassword] = useState("");
-
   const [addRestInputs, setAddRestInputs] = useState({
     id: "",
     img: "",
@@ -41,27 +35,12 @@ const toggleModal = () => {
 
 const handleAddRest = async (event:any) => {
   event.preventDefault()
-  // const linkImg = event.target[0].value
-  // const restName = event.target[1].value
-  // const chefName = event.target[2].value
-  // const rating1 = event.target[3].checked
-  // const rating2 = event.target[4].checked
-  // const rating3 = event.target[5].checked
-  // const rating4 = event.target[6].checked
-  // const rating5 = event.target[7].checked
-  // const openHours = event.target[8].value
-  // const closeHours = event.target[9].value
-  // const address = event.target[10].value
-  // const popularYes = event.target[11].checked
-  // const popularNo = event.target[12].checked
-  // const isNewYes = event.target[13].checked
-  // const isNewNo = event.target[14].checked
   setAddRestInputs({
     id: "1",
     img: event.target[0].value,
     name: event.target[1].value,
     chef: event.target[2].value,
-    rating: "1",
+    rating: event.target[3].value,
     openHour: event.target[8].value,
     closeHour: event.target[9].value,
     address: event.target[10].value,
@@ -70,37 +49,10 @@ const handleAddRest = async (event:any) => {
     isNew: true,
     isOpen: true,
     map:false
-    // rating1 = event.target[3].checked,
-    // rating2 = event.target[4].checked,
-    // rating3 = event.target[5].checked,
-    // rating4 = event.target[6].checked,
-    // rating5 = event.target[7].checked,
-    // popularYes = event.target[11].checked,
-    // popularNo = event.target[12].checked,
-    // isNewYes = event.target[13].checked,
-    // isNewNo = event.target[14].checked
   })
-//   await fetch("http://localhost:8000/epicure/restaurantsPage", {
-//     method: 'POST',
-//     body: JSON.stringify(addRestInputs),
-//     headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//         },
-//     })
-//     .then ((response) => {
-//     return response.json()
-//     }) 
-//     .then ((data) => {
-//         return data;
-//     }) 
-//     .catch((error)=> {
-//         console.log(error.massage);
-//     })
   setCount(prevCount => prevCount + 1);
-  // window.location.href = "http://localhost:3000/RestaurantsPage";
+  window.location.href = "http://localhost:3000/RestaurantsPage";
 }
-
-  // console.log(event, linkImg, restName, chefName, rating1, rating2,rating3,rating4,rating5, openHours,closeHours, address, popularYes, popularNo, isNewYes, isNewNo)
 
   useEffect(()=> {
     console.log("use effect happened");
@@ -133,7 +85,6 @@ const allRest = document.getElementById('all-rest-page-container')
 
   } else {
     document.body.classList.remove('active-modal')
-    // allRest!.style.display = 'grid';
   }
     
     return (
@@ -163,27 +114,34 @@ const allRest = document.getElementById('all-rest-page-container')
             <form onSubmit={handleAddRest} >
                 <div className="inputs-content" >
                   <div className="img-div">
-                    img: 
-                    <label><input type="text" name="name" id="" placeholder="Enter Img Link" /></label> 
+                  <select id="img-input" name="img">
+                  <option value="">Please select an image</option>
+                    <option value="claro-1">1</option>
+                    <option value="Lumina-2">2</option>
+                    <option value="tiger-lily-3">3</option>
+                    <option value="onza-4">4</option>
+                    <option value="kitchen-market-5">5</option>
+                    <option value="mashya-6">6</option>
+                    <option value="kab-kem-7">7</option>
+                    <option value="messa-8">8</option>
+                    <option value="nitan-thai-9">9</option>
+                    <option value="ya-pan">10</option>
+                  </select>
+                    {/* <label><input type="text" name="name" id="" placeholder="Enter Img Link" className="input-add-rest" /></label>  */}
                   </div>
-                
-                  <label><input type="text" name="name" id="" placeholder="Restaurant Name" /></label>                
-                  <label><input type="text" name="chefName" id="" placeholder="Chef Name" /></label>                
-                  Rating:
-                  <label><input type="radio" name="rating" id="" value="1"  /><span>1</span></label>                
-                  <label><input type="radio" name="rating" id=""  value="2"  /><span>2</span></label>                
-                  <label><input type="radio" name="rating" id="" value="3"   /><span>3</span></label>                
-                  <label><input type="radio" name="rating" id=""  value="4"  /><span>4</span></label>                
-                  <label><input type="radio" name="rating" id="" value="5"   /><span>5</span></label>                
-                  <p>openHours:</p>
+                  <label><input type="text" name="name" id="" placeholder="Restaurant Name" className="input-add-rest" /></label>                
+                  <label><input type="text" name="chefName" id="" placeholder="Chef Name" className="input-add-rest" /></label>                
+                  Rating (Scale 1-5):
+                  <label><input type="text" name="rating" id="" placeholder="Restaurant Rating" className="input-add-rest" /></label>                
+                  <h2>Open Hours:</h2>
                   From:
                   <input type="time" id="appt" name="appt"
                   min="01:00" max="18:00" required/>
                   To:
                   <input type="time" id="appt" name="appt"
                   min="01:00" max="18:00" required/>
-                  <label><input type="text" name="chefName" id="" placeholder="Restaurant Address" /></label> 
-                  {/* <label>Dishes:</label>   */}
+                  <label><input type="text" name="chefName" id="" placeholder="Restaurant Address" className="input-add-rest" /></label> 
+                  <label><input type="text" name="DishesName" id="" placeholder="Restaurant Dishes" className="input-add-rest" /></label> 
                   Popular Restauarnt:
                   <label><input type="radio" name="isPopular" id="" value="yes" /><span>Yes</span></label>                
                   <label><input type="radio" name="isPopular" id="" value="no"/><span>No</span></label>                
@@ -193,8 +151,8 @@ const allRest = document.getElementById('all-rest-page-container')
                   <button type="submit" id="button-add-rest">Add Restaurant</button>
                 </div>
             </form> 
-            <button className="close-modal" onClick={toggleModal}>
-            CLOSE
+            <button className="close-modal" onClick={toggleModal} >
+            X
             </button>
           </div>
         </div>
