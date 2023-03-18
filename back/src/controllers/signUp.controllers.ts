d21de28e7ab21478df79d269b0188c7a5b2a93dd
rotem huga <rotem.3007@gmail.com>
2023-03-18 21:44:40 +0200

    working, send tha user data only once to the DB - 18.03

--- back/src/controllers/signUp.controllers.ts
+++ back/src/controllers/signUp.controllers.ts
@@ -1,14 +1,14 @@
 import {getSignUp} from "../services/signUp.service"
 import express, {Request, Response} from "express";
-import { signUpModel } from "../model/signUp.model";
 import bcrypt from "bcrypt";
+import { request } from "http";
 const jwt = require('jsonwebtoken');
 
 export const getControllSignUp = async (req: Request, res: Response) => {
     console.log("users controller connected");
     try {
         const allDataUser = req.body
-        // const _usersCheck = await getSignUp(allDataUser);
+        const _usersCheck = await getSignUp(allDataUser);
         if (!(allDataUser.emailAddress && allDataUser.password && allDataUser.firstName && allDataUser.lastName && allDataUser.shippingAddress)) {
             res.status(400).send("All input is required");
           }
@@ -19,18 +19,18 @@ export const getControllSignUp = async (req: Request, res: Response) => {
         return res.status(409).send("User Already Exist. Please Login");
     }
     //Encrypt user password
-    const encryptedPassword = await bcrypt.hash(allDataUser.password, 10);
+    const encryptedPassword = await bcrypt.hash(allDataUser.password!, 10);
     
     // Create user in our database
     const user = await allDataUser.create({
         firstName:allDataUser.firstName,
         lastName:allDataUser.lastName,
-        emailAddress:allDataUser.emailAddress.toLowerCase(),
+        emailAddress:allDataUser.emailAddress!.toLowerCase(),
         shippingAddress:allDataUser.shippingAddress,
         password: encryptedPassword,
       });
 
-      res.status(201).json(user);
+      res.status(201).json(allDataUser);
     } catch (err) {
       console.log(err);
     }
--- front/src/component/SignUpPage/SignUpPage.tsx
+++ front/src/component/SignUpPage/SignUpPage.tsx
@@ -6,86 +6,90 @@ import { useState } from "react";
 import Navbar from "../navbar/all_navbar/navbar";
 
 
-// const SignUpPage = () => {
-//     const [allUserInputs, setAllUserInputs] = useState({
-//         firstName: "",
-//         lastName: "",
-//         emailAddress: "",
-//         shippingAddress: "",
-//         password: "",
-//     })
+const SignUpPage = () => {
+    const [count, setCount] = useState(0)
+    const [allUserInputs, setAllUserInputs] = useState({
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        shippingAddress: "",
+        password: "",
+    })
 
-//     useEffect(()=> {
-//         fetch("http://localhost:8000/epicure/signup/users", {
-//             method: 'POST',
-//             body: JSON.stringify(allUserInputs),
-//             headers: {
-//                 'Content-type': 'application/json; charset=UTF-8',
-//             },
-//         })
-//         .then ((response) => {
-//             return response.json()
-//         }) 
-//         .then ((data) => {
-//             console.log(data); // log the response data to the console
-//         }) 
-//         .catch((error)=> {
-//             console.log(error.massage);
-//         })
-//     },[allUserInputs])
-    
-//     const handleInputChange = async (event:any) => {
-//         event.preventDefault()
-//         console.log(event)        
-//         setAllUserInputs({
-//             firstName:  event.target[0].value,
-//             lastName: event.target[1].value,
-//             emailAddress: event.target[2].value,
-//             shippingAddress: event.target[3].value,
-//             password: event.target[4].value,
-//         }) 
-//         // console.log(allUserInputs)      
-//     }; 
+    const handleInputChange = async (event:any) => {
+        event.preventDefault()
+        setAllUserInputs({
+            firstName:  event.target[0].value,
+            lastName: event.target[1].value,
+            emailAddress: event.target[2].value,
+            shippingAddress: event.target[3].value,
+            password: event.target[4].value,
+        }) 
+        setCount(prevCount => prevCount + 1);
+        console.log(allUserInputs)      
+    }; 
+
+    useEffect(()=> {
+        console.log(allUserInputs)      
+        if (count >= 1) {
+            fetch("http://localhost:8000/epicure/users", {
+                method: 'POST',
+                body: JSON.stringify(allUserInputs),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then ((response) => {
+                return response.json()
+            }) 
+            .then ((data) => {
+                console.log(data); // log the response data to the console
+            }) 
+            .catch((error)=> {
+                console.log(error.massage);
+            })
+            }
+    },[allUserInputs, count])
         
-//     return (
-//         <>
-//             <div>
-//                 <Navbar />
-//             </div>
-//             <form className="Sign-in-container" onSubmit={handleInputChange}>
-//                 <div className="sign-in-title">SIGN UP</div>
-//                 <div className="sign-in-sub-title">To continue order, please sign up</div>
-//                 <div className="names-container">
-//                     <div className="first-name-container">
-//                         <input type="text" className="user-first-name" placeholder={"First Name"}/>
-//                     </div>
-//                     <div className="last-name-container">
-//                         <input type="text" className="user-last-name" placeholder={"Last Name"}/>
-//                     </div>
-//                 </div>
-//                 <div>
-//                     <div className="email-container">
-//                         <input type="text" className="user-email" placeholder={"Email Adress"} />
-//                     </div>
-//                 </div>
-//                 <div>
-//                     <div className="address-container">
-//                         <input type="text" className="user-address" placeholder="Shipping Address"/>
-//                     </div>
-//                 </div>
-//                 <div>
-//                     <div className="password-container">
-//                         <input type="password" className="user-password" placeholder={"Password"} />
-//                     </div>
-//                 </div>
-//                 <div className="sign-up-container">
-//                     <button type="submit" className="sign-up-button">SIGN UP</button>
-//                 </div>
-//             </form>
-//         </>
-//     )   
-// }
-// export default SignUpPage 
+    return (
+        <>
+            <div>
+                <Navbar />
+            </div>
+            <form className="Sign-in-container" onSubmit={handleInputChange}>
+                <div className="sign-in-title">SIGN UP</div>
+                <div className="sign-in-sub-title">To continue order, please sign up</div>
+                <div className="names-container">
+                    <div className="first-name-container">
+                        <input type="text" className="user-first-name" placeholder={"First Name"}/>
+                    </div>
+                    <div className="last-name-container">
+                        <input type="text" className="user-last-name" placeholder={"Last Name"}/>
+                    </div>
+                </div>
+                <div>
+                    <div className="email-container">
+                        <input type="text" className="user-email" placeholder={"Email Adress"} />
+                    </div>
+                </div>
+                <div>
+                    <div className="address-container">
+                        <input type="text" className="user-address" placeholder="Shipping Address"/>
+                    </div>
+                </div>
+                <div>
+                    <div className="password-container">
+                        <input type="password" className="user-password" placeholder={"Password"} />
+                    </div>
+                </div>
+                <div className="sign-up-container">
+                    <button type="submit" className="sign-up-button">SIGN UP</button>
+                </div>
+            </form>
+        </>
+    )   
+}
+export default SignUpPage 
 
 // import React, { useEffect } from "react";
 // import "./SignUpPage.css";
@@ -95,110 +99,104 @@ import Navbar from "../navbar/all_navbar/navbar";
 // import Navbar from "../navbar/all_navbar/navbar";
 
 
-const SignUpPage:any  = (data:any) => {
-    const [allUserInputs, setAllUserInputs] = useState({
-        firstName: "",
-        lastName: "",
-        emailAddress: "",
-        shippingAddress: "",
-        password: "",
-    })
+// const SignUpPage:any  = (data:any) => {
+//     const [count, setCount] = useState(0)
+//     const [allUserInputs, setAllUserInputs] = useState({
+//         firstName: "",
+//         lastName: "",
+//         emailAddress: "",
+//         shippingAddress: "",
+//         password: "",
+//     })
 
-    useEffect(()=> {
-        // count ++
-        // if(count>1)
-        console.log("use effect happend");
-        console.log(allUserInputs);
-
-     fetch("http://localhost:8000/epicure/signup/users", {
-            method: 'POST',
-            body: JSON.stringify(allUserInputs),
-            headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-                },
-            })
-            .then ((response) => {
-            return response.json()
-            }) 
-            .then ((data) => {
-                return data;
-            }) 
-            .catch((error)=> {
-                console.log(error.massage);
-            })
-    },[allUserInputs])
+//     useEffect(()=> {
+//         console.log("use effect happened");
+//         console.log(allUserInputs)
+//         if (count >= 1) {
+//           fetch("http://localhost:8000/epicure/signup/users", {
+//             method: 'POST',
+//             body: JSON.stringify(allUserInputs),
+//             headers: {
+//               'Content-type': 'application/json; charset=UTF-8',
+//             },
+//           })
+//           .then((response) => {
+//             return response.json();
+//           }) 
+//           .then((data) => {
+//             return data;
+//           }) 
+//           .catch((error)=> {
+//             console.log(error.message);
+//           });
+//         }
+//       }, [allUserInputs, count]);
     
-    const handleInputChange = async (event:any) => {
-        event.preventDefault()
-            console.log(event)        
-            setAllUserInputs({
-            firstName:  event.target[0].value,
-            lastName: event.target[1].value,
-            emailAddress: event.target[2].value,
-            shippingAddress: event.target[3].value,
-            password: event.target[4].value,
-        }) 
-        
-    //     const [emailError, setEmailError] = useState('')
-    //     const validateEmail = (e:any) => {
-    //     const email = e.target.value
-    //     if (validator.isEmail(email)) {
-    //     setEmailError('Valid Email')
-    //     } else {
-    //     setEmailError('Enter valid Email!')
-    //     }
-    // }
-    
-// function userInputsFunc() {
+//     const handleInputChange = async (event:any) => {
+//         event.preventDefault()
+//             setAllUserInputs({
+//             firstName:  event.target[0].value,
+//             lastName: event.target[1].value,
+//             emailAddress: event.target[2].value,
+//             shippingAddress: event.target[3].value,
+//             password: event.target[4].value,  
+//         })
+//         setCount(prevCount => prevCount + 1);
 
- return (
-    <>
-    <div>
-        <Navbar />
-    </div>
-    <form className="Sign-in-container" onSubmit={handleInputChange}>
-        <div className="sign-in-title">SIGN UP</div>
-        <div className="sign-in-sub-title">To conitnue order, please sign in</div>
-        <div className="names-container">
-            <div className="first-name-container">
-                <input type="text" className="user-first-name" placeholder={"First Name"}/>
-            </div>
-            <div className="last-name-container">
-                <input type="text" className="user-last-name" placeholder={"Last Name"}/>
-            </div>
-        </div>
-        <div>
-            <div className="email-container">
-                <input type="text" className="user-email" placeholder={"Email Adress"}
-                    // onChange={(e) => validateEmail(e)}
-                    >
-                </input>
-                    {/* {emailError} */}
-            </div>
-        </div>
-        <div>
-            <div className="address-container">
-                <input type="text" className="user-address" placeholder="Shipping Address"/>
-            </div>
-        </div>
-        <div>
-            <div className="password-container">
-                <input type="text" className="user-password" placeholder={"Password"}/>
-            </div>
-        </div>
-        {/* <div>
-            <div className="password-confirm-container">
-                <input type="text" className="user-password" placeholder={"Confirm Password"}/>
-            </div>
-        </div> */}
-        <div className="sign-up-container">
-            {/* <button type="submit" className="sign-up-button" onClick={userInput}>SIGN UP</button> */}
-            <button type="submit" className="sign-up-button">SIGN UP</button>
-        </div>
-    </form>
-    </>
- )   
-}
-}
-export default SignUpPage  
+//     //     const [emailError, setEmailError] = useState('')
+//     //     const validateEmail = (e:any) => {
+//     //     const email = e.target.value
+//     //     if (validator.isEmail(email)) {
+//     //     setEmailError('Valid Email')
+//     //     } else {
+//     //     setEmailError('Enter valid Email!')
+//     //     }
+//     // }
+    
+// // function userInputsFunc() {
+
+//  return (
+//     <>
+//     <div>
+//         <Navbar />
+//     </div>
+//     <form className="Sign-in-container" onSubmit={handleInputChange}>
+//         <div className="sign-in-title">SIGN UP</div>
+//         <div className="sign-in-sub-title">To conitnue order, please sign in</div>
+//         <div className="names-container">
+//             <div className="first-name-container">
+//                 <input type="text" className="user-first-name" placeholder={"First Name"}/>
+//             </div>
+//             <div className="last-name-container">
+//                 <input type="text" className="user-last-name" placeholder={"Last Name"}/>
+//             </div>
+//         </div>
+//         <div>
+//             <div className="email-container">
+//                 <input type="text" className="user-email" placeholder={"Email Adress"}
+//                     >
+//                 </input>
+//                     {/* {emailError} */}
+//             </div>
+//         </div>
+//         <div>
+//             <div className="address-container">
+//                 <input type="text" className="user-address" placeholder="Shipping Address"/>
+//             </div>
+//         </div>
+//         <div>
+//             <div className="password-container">
+//                 <input type="text" className="user-password" placeholder={"Password"}/>
+//             </div>
+//         </div>
+//         <div className="sign-up-container">
+//             {/* <button type="submit" className="sign-up-button" onClick={userInput}>SIGN UP</button> */}
+//             <button type="submit" className="sign-up-button">SIGN UP</button>
+//         </div>
+//     </form>
+//     </>
+//  )   
+// }
+// }
+// export default SignUpPage  
 
